60872f329db88c89f12fcf23c174ea50d724ca7d
chan_prakash <chandrakanthprakash26@gmail.com>
2024-12-16 15:46:55 +0530

    Update buyandsellstocks.java

--- buyandsellstocks.java
+++ buyandsellstocks.java
@@ -1,5 +1,23 @@
 package leetcode;
 
 public class buyandsellstocks {
+  public static int maxProfit(int[] prices) {
+	        int minPrice = Integer.MAX_VALUE;
+	        int maxProfit = 0;
+	        
+	        for (int i = 0; i < prices.length; i++) {
+	            if (prices[i] < minPrice) {
+	                minPrice = prices[i];
+	            }
+	            int profit = prices[i] - minPrice;
+	            if (profit > maxProfit) {
+	                maxProfit = profit;
+	            }
+	        }
+	        
+	        return maxProfit;
+	    }
+	} 
+  
 
 }
