708254333e7402b0cefe59049f2b4cffc0e663f1
chan_prakash <chandrakanthprakash26@gmail.com>
2025-01-09 21:56:43 +0530

    Add files via upload

---
+++ sortingcheck.java
@@ -0,0 +1,16 @@
+package optimalsolution;
+
+public class sortingcheck {
+
+class Solution {
+    public boolean arraySortedOrNot(int[] arr) {
+        // code here
+        for(int i =1;i<arr.length;i++){
+            if(arr[i]<arr[i-1]) return false;
+            
+        }
+         return true;
+    }
+}
+
+}
